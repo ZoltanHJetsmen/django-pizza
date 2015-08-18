2236aa67541d7bbe5cd93d549d12ecbd1659acb6
Zoltan Hirata Jetsmen <zoltan.jetsmen@usp.br>
2015-08-18 11:00:31 -0300

    Added loops.py

--- pyexamples/loops.py
+++ pyexamples/loops.py
@@ -15,6 +15,8 @@ pedidos = [
 ]
 
 for pedido in pedidos:
+    s = 'Nome: {}\nSabor: {}'
+    print(s.format(pedido['nome'], pedido['sabor']))
     print('Nome: {0} Sabor: {1}'.format(pedido['nome'], pedido['sabor'])) 
     print(pedido['nome'], pedido['sabor'])
 
