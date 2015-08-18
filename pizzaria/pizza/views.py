630a9884aaeb5e750c271a64c02f9dbe65db8611
Zoltan Hirata Jetsmen <zoltan.jetsmen@usp.br>
2015-08-18 17:17:34 -0300

    Added views.py

--- pizzaria/pizza/views.py
+++ pizzaria/pizza/views.py
@@ -1,10 +1,9 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from .models import Pizza
+
 def menu(request):  
     pizzas = Pizza.objects.all()
-    result = ''
-    for pizza in pizzas:
-        result += pizza.name
-        result += '<br>'
-    return HttpResponse('Hello world') 
+    context = {'pizzas': pizzas}
+    return render(request, 'pizza/menu.html', context) 
