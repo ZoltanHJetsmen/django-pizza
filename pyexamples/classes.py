c64c411e34dedb1da282a21f3e3625d54acc61fe
Zoltan Hirata Jetsmen <zoltan.jetsmen@usp.br>
2015-08-18 14:26:23 -0300

    Added classes example

--- pyexamples/classes.py
+++ pyexamples/classes.py
@@ -1,5 +1,5 @@
 class Pedido(object):
-    def __init__(self, nome, endereco, observacoes):
+    def __init__(self, nome, endereco, observacoes = None):
         self.nome = nome
         self.endereco = endereco
         self.observacoes = observacoes
