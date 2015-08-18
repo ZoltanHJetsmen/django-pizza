789180aa4e1410b2ab1c520c54dfb81bc0790723
Zoltan Hirata Jetsmen <zoltan.jetsmen@usp.br>
2015-08-18 16:55:06 -0300

    Added views.py

--- pizzaria/pizza/views.py
+++ pizzaria/pizza/views.py
@@ -2,4 +2,9 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def menu(request):  
+    pizzas = Pizza.objects.all()
+    result = ''
+    for pizza in pizzas:
+        result += pizza.name
+        result += '<br>'
     return HttpResponse('Hello world') 
