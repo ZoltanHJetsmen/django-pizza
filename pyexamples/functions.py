9b4e9a436be8e470a38ca475bc7f493d1ea3929d
Zoltan Hirata Jetsmen <zoltan.jetsmen@usp.br>
2015-08-18 11:39:46 -0300

    Added function example

--- pyexamples/functions.py
+++ pyexamples/functions.py
@@ -1,14 +1,19 @@
 pedidos = []
 
 
-def adicionaPedidos(nome, sabor):
+def adicionaPedidos(nome, sabor, observacao='None'):
     pedido = {}
     pedido['nome'] = nome
     pedido['sabor'] = sabor
+    pedido['observacao'] = observacao
+    return (pedido)
 
-    pedidos.append(pedido)
+pedidos.append(adicionaPedidos('mario', 'pepperoni'))
+pedidos.append(adicionaPedidos('marco', 'portuguesa', 'dobro de presunto'))
 
-print(pedidos)
-adicionaPedidos('mario', 'pepperoni')
-adicionaPedidos('marco', 'portuguesa')
-print(pedidos)
+for pedido in pedidos:
+    template = 'Nome: {nome}\nSabor: {sabor}'
+    print(template.format(**pedido))
+    if pedido['observacao']:
+        print('Observacao: {}'.format(pedido['observacao']))
+    print('-'*20)
